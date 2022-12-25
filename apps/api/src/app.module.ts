1cc5d883861b1e64c38a4e698bb67558876634c7
Kiran Ojha <kiranojha1226@gmail.com>
2022-12-25 13:46:48 +0545

    feat(api): validate and enable env

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { validationSchemaForEnv } from './config/environment-variables';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    validationSchema: validationSchemaForEnv,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ apps/api/src/config/environment-variables.ts
@@ -0,0 +1,11 @@
+import * as Joi from 'joi';
+
+export interface EnvironmentVariables {
+  PORT: string;
+  DATABASE_URL: string;
+}
+
+export const validationSchemaForEnv = Joi.object<EnvironmentVariables, true>({
+  PORT: Joi.string().required(),
+  DATABASE_URL: Joi.string().required(),
+});
